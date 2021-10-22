4227afcc4737dac5289fa708d2bde8948ccbfcb6
kimohgeun <kog1059@gmail.com>
2021-10-22 18:18:09 +0900

    Setting : 리액트 + 타입스크립트 개발 환경 설정

---
+++ webpack.config.dev.ts
@@ -0,0 +1,50 @@
+import path from "path";
+import { Configuration, HotModuleReplacementPlugin } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+
+const config: Configuration = {
+  mode: "development",
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+    new HotModuleReplacementPlugin(),
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    port: 3000,
+    historyApiFallback: true,
+    open: true,
+    hot: true,
+  },
+};
+
+export default config;
