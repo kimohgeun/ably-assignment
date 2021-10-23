c0b3fcb670cd1c4134abd169e9fe5a0c9ae8f8e3
kimohgeun <kog1059@gmail.com>
2021-10-24 03:12:28 +0900

    Update : Router 컴포넌트 로그인 상황 로직 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,15 +1,33 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useEffect } from "react";
+import { useUser } from "~/hooks/store";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 import { LoginPage, ResetPasswordPage } from "~/pages";
 
 function Router() {
+  const { token: storeToken, setToken } = useUser();
+  const localStorageToken = localStorage.getItem("ABLY_TOKEN");
+
+  useEffect(() => {
+    localStorageToken && setToken(localStorageToken);
+  }, []);
+
+  if (localStorageToken && !storeToken) return null;
   return (
     <BrowserRouter>
       <Suspense fallback="로딩중...">
         <Switch>
-          <Route exact path="/login" component={LoginPage} />
-          <Route exact path="/reset-password" component={ResetPasswordPage} />
-          <Redirect path="*" to="/login" />
+          {storeToken ? (
+            <>
+              <Route path="/user" component={() => <div>유저</div>} />
+              <Redirect path="*" to="/user" />
+            </>
+          ) : (
+            <>
+              <Route path="/login" component={LoginPage} />
+              <Route path="/reset-password" component={ResetPasswordPage} />
+              <Redirect path="*" to="/login" />
+            </>
+          )}
         </Switch>
       </Suspense>
     </BrowserRouter>
